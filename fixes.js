a9694da7ed91756319d62d3c4245676d6b77caec
Christine Wang <wang.christiney@gmail.com>
2017-09-15 11:28:04 -0700

    first commit

---
+++ fixes.js
@@ -0,0 +1,52 @@
+
+// If last string in URL matches this, do something
+var url = window.location.href;
+
+if (getLastPart(url) === "articles") {
+    // Do something
+}
+
+function getLastPart(url) {
+    var parts = url.split("/");
+    return (url.lastIndexOf('/') !== url.length - 1 ?
+        parts[parts.length - 1] :
+        parts[parts.length - 2]);
+}
+
+
+// Fix sticky header
+var content = document.querySelector('#main-content');
+var header = document.querySelector('#placeholder');
+
+if (content && header) {
+    window.addEventListener('scroll', function fixStickyHeader() {
+        if (header.style.display === 'block') {
+            content.style.marginTop = '35px';
+        } else {
+            content.style.marginTop = '0';
+        }
+    })
+}
+
+
+// If URL does not contain this string, do somethingjQuery(function() {
+    $('.imagecontainer').on('click', function() {
+        setTimeout(function() {
+            $(document).ready(function() {
+                if (document.location.href.indexOf('ford-gt') === -1) {
+                    // Do something
+                }
+            });
+        }, 250)
+    })
+});
+
+
+// Add class to first two li elements on click
+jQuery(function(){
+  $(".imagecontainer").on('click', function() {
+      setTimeout(function(){
+          $('.modal .slideshow .slides .slide').slice(0,2).addClass('ggnoads')
+      }, 250)
+  })
+});
